41b8b6c348b7881ab8f295f7d9cb9439ec0b7835
pooja <kamblepooja24.pk@gmail.com>
2019-12-21 18:33:39 +0530

    Updates

---
+++ precache-manifest.aa979c4b7b68f8ddf0a48d07374f119d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e661e1e17c7b2c1ab68d0288af478714",
+    "url": "/RoboFriend/index.html"
+  },
+  {
+    "revision": "f83668a4a00c60036b64",
+    "url": "/RoboFriend/static/css/main.dd286628.chunk.css"
+  },
+  {
+    "revision": "7355c14aaf62b74826b9",
+    "url": "/RoboFriend/static/js/2.b02be2e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RoboFriend/static/js/2.b02be2e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f83668a4a00c60036b64",
+    "url": "/RoboFriend/static/js/main.528f7f25.chunk.js"
+  },
+  {
+    "revision": "3861c34a330d9f9c8984",
+    "url": "/RoboFriend/static/js/runtime-main.24220032.js"
+  }
+]);
